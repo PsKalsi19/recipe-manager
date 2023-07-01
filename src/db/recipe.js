07f23ba562b75f486c4ab70acdccfed363932ab8
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-07-01 19:14:36 +0530

    first commit

---
+++ src/db/recipe.js
@@ -0,0 +1,51 @@
+const recipes = [
+  {
+    name: "Chicken Tikka Masala",
+    cuisine: "Indian",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/f/fd/Chicken_tikka_masala.jpg",
+    ingredients: [
+      "1 pound boneless, skinless chicken breasts, cut into 1-inch pieces",
+      "1 tablespoon olive oil",
+      "1 onion, chopped",
+      "2 cloves garlic, minced",
+      "1 tablespoon ginger, grated",
+      "1 teaspoon ground cumin",
+      "1 teaspoon ground coriander",
+      "1 teaspoon turmeric powder",
+      "1 teaspoon garam masala",
+      "1/2 teaspoon cayenne pepper",
+      "1 (15 ounce) can tomato sauce",
+      "1 (15 ounce) can low-fat or fat-free coconut milk",
+      "1/2 cup heavy cream",
+      "Salt and pepper to taste",
+    ],
+    description:
+      "This creamy and flavorful chicken tikka masala is a classic Indian dish that is perfect for a weeknight meal. The chicken is cooked in a tomato-based sauce with spices like cumin, coriander, and turmeric. The sauce is then thickened with coconut milk and heavy cream, and the dish is finished with salt and pepper to taste. Serve with rice or naan bread for a complete meal.",
+  },
+  {
+    name: "Kung Pao Chicken",
+    cuisine: "Chinese",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c2/Kung-pao-shanghai.jpg",
+    ingredients: [
+      "1 pound boneless, skinless chicken breasts, cut into 1-inch pieces",
+      "1 tablespoon cornstarch",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon black pepper",
+      "1 tablespoon vegetable oil",
+      "1/2 cup chopped onion",
+      "1/2 cup chopped green bell pepper",
+      "1/4 cup chopped peanuts",
+      "2 tablespoons soy sauce",
+      "2 tablespoons rice vinegar",
+      "1 tablespoon sugar",
+      "1 teaspoon sesame oil",
+      "1/4 teaspoon red pepper flakes",
+    ],
+    description:
+      "This classic Chinese dish is made with chicken that is coated in a crispy cornstarch mixture and then stir-fried with vegetables and a sweet and savory sauce. The chicken is first coated in a mixture of cornstarch, salt, and pepper. Then, it is stir-fried in hot oil until it is cooked through. The vegetables are then added to the pan and cooked until they are softened. Finally, the sauce is added and the dish is cooked until it is heated through. Serve with rice or noodles for a delicious and satisfying meal.",
+  },
+];
+
+export default recipes;
